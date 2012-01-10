f6d4f6ac23181e96bb1d5418f6d2161af465a4ed
aomoriringo <sel0433@gmail.com>
2012-01-11 08:45:12 +0900

    add csvreader

--- numberbot.py
+++ numberbot.py
@@ -4,6 +4,33 @@ import httplib2
 import simplejson
 import oauth2
 import twitter
+import csv
+
+
+
+class UnicodeCsvReader(object):
+    def __init__(self, f, encoding="utf-8", **kwargs):
+        self.csv_reader = csv.reader(f, **kwargs)
+        self.encoding = encoding
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        # read and split the csv row into fields
+        row = self.csv_reader.next() 
+        # now decode
+        return [unicode(cell, self.encoding) for cell in row]
+
+    @property
+    def line_num(self):
+        return self.csv_reader.line_num
+
+
+class UnicodeDictReader(csv.DictReader):
+    def __init__(self, f, encoding="utf-8", fieldnames=None, **kwds):
+        csv.DictReader.__init__(self, f, fieldnames=fieldnames, **kwds)
+        self.reader = UnicodeCsvReader(f, encoding=encoding, **kwds)
 
 
 class NumberBot(webapp.RequestHandler):
@@ -16,6 +43,8 @@ class NumberBot(webapp.RequestHandler):
         self.ACCOUNT_NAME = "number_bot"
         self.PASSWORD = "numbers"
 
+
+
     # login (with OAuth authentication)
     def login(self):
         self.api = twitter.Api(cache=None,
@@ -25,7 +54,7 @@ class NumberBot(webapp.RequestHandler):
                                access_token_secret = self.ACCESS_TOKEN_SECRET)
 
 
-    def get_allfriend_ids(self):        
+    def get_allfriend_ids(self):
         cursor = "-1"
         friend_ids = []
         
@@ -51,6 +80,7 @@ class NumberBot(webapp.RequestHandler):
             cursor = data["next_cursor_str"]
 
         return follow_ids
+ 
     
     def modify_followers(self):
         followers = self.get_allfollower_ids()
@@ -64,10 +94,10 @@ class NumberBot(webapp.RequestHandler):
         self.response.out.write( len( friends_set ) )
         self.response.out.write( "\n\n" )
         
-        #フォロー対象
+        #follow target members
         non_friends = followers_set - friends_set
                     
-        #リムーブ対象
+        #remove target members
         bye_friends = friends_set - followers_set
 
         self.response.out.write( "follow -> ")
@@ -89,8 +119,28 @@ class NumberBot(webapp.RequestHandler):
             except Exception, e:
                 self.response.out.write( e.message )
                 self.response.out.write( "\n" )
+
+
+    def createDictionary(self):
         
-                  
+        numData = UnicodeCsvReader( open('numbers.csv', 'r'))
+        
+        self.numbers = dict()
+        for row in numData:
+            #self.numbers[int(row[1])] = row[0]
+            
+            #for col in row:
+            #    self.response.out.write(col)
+            #    self.response.out.write( '\n' ) 
+            #self.response.out.write(row[0])
+            #self.response.out.write(row[1])
+            #self.response.out.write(row[2])
+            self.numbers.update({row[1]:row[0]})
+        
+        self.response.out.write(self.numbers)
+        self.response.out.write( self.numbers[' 1000'])
+        
+        
     def get(self):
         
         # for test
@@ -102,11 +152,11 @@ class NumberBot(webapp.RequestHandler):
         self.response.out.write( self.api.VerifyCredentials() )
         self.response.out.write( "\n" )
 
-        # friends/followers同期
-        self.modify_followers()
+        # friends/followers synchronize
+        #self.modify_followers()
          
         #api.PostUpdate('test')
-        
+        self.createDictionary()
 
 application = webapp.WSGIApplication([('/', NumberBot)], debug=True)    
     
